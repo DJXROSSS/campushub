003917b188343b1c4dd7738ba436b4b2e810d3a3
Jatin Kumar <jatinkumar8125@gmail.com>
2026-03-23 02:35:32 +0530

    added reactive background using spline

---
+++ components/SplineBackground.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import Spline from '@splinetool/react-spline'
+
+export default function SplineBackground() {
+  return (
+    <Spline
+      className="w-full h-full"
+      scene="https://prod.spline.design/7xhseSs9TzZQol9q/scene.splinecode"
+    />
+  )
+}
--- components/landing/landingpage.tsx
+++ components/landing/landingpage.tsx
@@ -1,10 +1,24 @@
+'use client'
+
 import React from "react"
 import Navbar from "./navbar"
+import dynamic from 'next/dynamic'
+
+const SplineBackground = dynamic(
+  () => import('../SplineBackground'),
+  { ssr: false }
+)
 
 const LandingPage = () => {
   return (
-    <div>
-      <Navbar />
+    <div className="relative w-full h-screen">
+      <div className="absolute inset-0 -z-10 pointer-events-none">
+        <SplineBackground />
+      </div>
+      <div className="relative z-10">
+        <Navbar />
+      </div>
+
     </div>
   )
 }
